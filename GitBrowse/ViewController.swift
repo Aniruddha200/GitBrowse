368d924f64ab8fe78193516f82498c8c1eaf54ba
Aniruddha Baidya <aniruddha.baidya@sundaymobility.com>
2022-07-07 14:22:07 +0530

    Initial Commit

---
+++ GitBrowse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitBrowse
+//
+//  Created by Sunday on 07/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
